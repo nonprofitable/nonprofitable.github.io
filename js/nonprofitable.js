7a39f7ba40addc7070d2c689d153df4fd9b9d3d5
Dave Chakrabarti <dave@nonprofitable.org>
2017-10-17 09:00:46 +0000

    JS bugfixes, homepage nav links.

--- js/nonprofitable.js
+++ js/nonprofitable.js
@@ -1,22 +1,19 @@
-// Rebuild two homepage nav links for prettier scrolling.
+// Rebuild two nav links for prettier scrolling when at the homepage.
+
+function checkHome {
+    return location.pathname == "/";    
+}
+
+//var is_root = location.pathname == "/"; //Equals true if we're at the root
 
 function removeCharacterAtIndex(value, index) {
   return value.substring(0, index) + value.substring(index + 1);
 }
 
-// Work link:
-
-var input = $( ".navbar-nav .page-scroll:eq( 0 )" ).attr( "href" );
-var newAnchor = (removeCharacterAtIndex(input, 0));
-
-$( ".navbar-nav .page-scroll:eq( 0 )").attr( "href", newAnchor);
-
-// Writing link:
-
-input = $( ".navbar-nav .page-scroll:eq( 1 )" ).attr( "href" );
-newAnchor = (removeCharacterAtIndex(input, 0));
-
-$( ".navbar-nav .page-scroll:eq( 1 )").attr( "href", newAnchor);
+if (checkHome()) {
+    $( ".navbar-nav .page-scroll:eq( 0 )").attr( "href", "#work");
+    $( ".navbar-nav .page-scroll:eq( 1 )").attr( "href", "#writing");
+}
 
 // Testing outut:
 // var test = $( ".navbar-nav .page-scroll").attr( "href" );
